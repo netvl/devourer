1ec64419843438607a5adce848e5ab9bcd3cbf07
Vladimir Matveev <vmatveev@citrine.cc>
2013-02-18 15:20:55 +0400

    Initial commit

---
+++ src/main/java/org/googolplex/devourer/Constants.java
@@ -0,0 +1,17 @@
+package org.googolplex.devourer;
+
+/**
+ * Date: 18.02.13
+ * Time: 15:16
+ */
+public final class Constants {
+    private Constants() {
+    }
+
+    public static final class Stacks {
+        private Stacks() {
+        }
+
+        public static final String MAIN_STACK = "main";
+    }
+}
---
+++ src/main/java/org/googolplex/devourer/contexts/AttributedContext.java
@@ -0,0 +1,15 @@
+package org.googolplex.devourer.contexts;
+
+import com.google.common.base.Optional;
+
+import java.util.List;
+
+/**
+ * Date: 18.02.13
+ * Time: 15:08
+ */
+public interface AttributedContext extends ElementContext {
+    List<String> attributeNames();
+    Optional<String> attribute(String name);
+    Optional<String> attribute(String name, String namespace);
+}
