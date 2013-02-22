f5e2fc2c365141bdaa490f080011f7ae38206d65
Vladimir Matveev <vmatveev@citrine.cc>
2013-02-22 18:18:59 +0400

    Started writing README

---
+++ src/test/java/org/googolplex/devourer/sandbox2/Login.java
@@ -0,0 +1,15 @@
+package org.googolplex.devourer.sandbox2;
+
+/**
+ * Date: 22.02.13
+ * Time: 17:49
+ */
+public class Login {
+    public final String site;
+    public final String value;
+
+    public Login(String site, String value) {
+        this.site = site;
+        this.value = value;
+    }
+}
